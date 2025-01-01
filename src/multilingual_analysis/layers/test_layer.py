71edc1502a333e930d28fb9b03fd6d2745cf06c5
Matthias Uhlig <matthias.uhlig@aol.de>
2025-01-01 20:03:16 +0100

    Rework_Layers

--- src/multilingual_analysis/layers/test_layer.py
+++ src/multilingual_analysis/layers/test_layer.py
@@ -1,12 +1,14 @@
 import logging
 import random
 import sys
-import torch
 
+import torch
+from langdetect import detect_langs
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from langdetect import detect_langs
+
 logging.basicConfig(level=logging.INFO)
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,10 +16,12 @@ import seaborn as sns
 
 random.seed(112)
 
+model_name = "HuggingFaceTB/SmolLM-135M"
+
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM-135M")
-model = AutoModelForCausalLM.from_pretrained("HuggingFaceTB/SmolLM-135M", device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name,torch_dtype=torch.bfloat16).to(device)
 
 
 def tracefunc(frame, event, arg, indent=None):
@@ -32,7 +36,9 @@ def tracefunc(frame, event, arg, indent=None):
 
 def Prompting(model, prompt, candidate_premature_layers):
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
-    hidden_states, outputs = model.generate(input_ids=inputs.input_ids, max_new_tokens=64,candidate_premature_layers=candidate_premature_layers)
+    hidden_states, outputs = model.generate(
+        input_ids=inputs.input_ids, max_new_tokens=64, candidate_premature_layers=candidate_premature_layers, output_hidden_states=True
+    )
     # hidden_states, outputs = model.generate(**{'input_ids':inputs.input_ids})
     hidden_embed = {}
     hidden_embed_token_level = {}
@@ -98,7 +104,7 @@ def layerwise_lang_distribution_th(lang_distribution):
 
 def average_layerwise_lang_distribution(lst_lang_distribution, candidate_langs=None):
     if candidate_langs is None:
-        candidate_langs = ["en", "zh", "es", "ru", "de", "fr"]
+        candidate_langs = ["en", "de"]
     average_lang_distribution = {}
     for lang_distribution in lst_lang_distribution:
         for layer in lang_distribution:
@@ -113,7 +119,7 @@ def average_layerwise_lang_distribution(lst_lang_distribution, candidate_langs=N
         for lang in average_lang_distribution[layer]:
             average_lang_distribution[layer][lang] /= len(lst_lang_distribution)
     for lang in candidate_langs:
-        if lang in ("en", "zh"):
+        if lang in ("en", "de"):
             average_lang_distribution[0][lang] = 0
         else:
             average_lang_distribution[0][lang] = 1 / (len(candidate_langs) - 2)
@@ -121,7 +127,10 @@ def average_layerwise_lang_distribution(lst_lang_distribution, candidate_langs=N
     return average_lang_distribution
 
 
-def plot_lang_distribution(lang_distribution, candidate_langs, candidate_layers) -> None:
+def plot_lang_distribution(lang_distribution, candidate_langs, candidate_layers, show_plot=False) -> None:
+    image_dir = Path.cwd() / "plots"
+    image_dir.mkdir(exist_ok=True)
+
     lang_distribution_matrix = []
     for layer in candidate_layers:
         lang_distribution_matrix.append([lang_distribution[layer][lang] for lang in candidate_langs])
@@ -132,47 +141,52 @@ def plot_lang_distribution(lang_distribution, candidate_langs, candidate_layers)
     plt.title("Layerwise Language Distribution")
     plt.xlabel("Layer")
     plt.ylabel("Language")
-    plt.show()
-    plt.savefig("lang_distribution.png")
+    plt.savefig(image_dir / "lang_distribution.png")
+    if show_plot:
+        plt.show()
 
 
 def main(argv) -> None:
     de_prompts = [
-    "Frage: Was sind die besten deutschen Filme? Antwort: ",
-    "Frage: Was sind die besten deutschen Bücher? Antwort: ",
-    "Frage: Wie kann ich meine Deutschkenntnisse verbessern? Antwort: ",
-    "Kann mir jemand ein gutes Restaurant in München empfehlen?",
-    "Was sind die besten Sehenswürdigkeiten in Berlin?",
-    "Wie kann ich meine Deutschkenntnisse verbessern?",
-    "Was sind die besten Tipps für einen guten Schlaf?",
-    "Wo kann ich in Hamburg gut shoppen?",
-    "Wie kann ich meine Zeit besser nutzen?",
-    "Was sind die besten deutschen Serien?",
-    "Was sind die besten deutschen Lieder?"
-    "Kannst du drei günstige Reiseziele in Österreich für Einzelreisende empfehlen?",
-    "Was sind effektive Strategien zur Stressbewältigung?"
+        "Frage: Was sind die besten deutschen Filme? Antwort: ",
+        # "Frage: Was sind die besten deutschen Bücher? Antwort: ",
+        # "Frage: Wie kann ich meine Deutschkenntnisse verbessern? Antwort: ",
+        # "Kann mir jemand ein gutes Restaurant in München empfehlen?",
+        # "Was sind die besten Sehenswürdigkeiten in Berlin?",
+        # "Wie kann ich meine Deutschkenntnisse verbessern?",
+        # "Was sind die besten Tipps für einen guten Schlaf?",
+        # "Wo kann ich in Hamburg gut shoppen?",
+        # "Wie kann ich meine Zeit besser nutzen?",
+        # "Was sind die besten deutschen Serien?",
+        # "Was sind die besten deutschen Lieder?"
+        # "Kannst du drei günstige Reiseziele in Österreich für Einzelreisende empfehlen?",
+        # "Was sind effektive Strategien zur Stressbewältigung?"
     ]
 
     en_prompts = [
-    "What are some popular tourist attractions in New York City?",
-    "How can I improve my English writing skills?",
-    "Can you recommend three must-read books from the science fiction genre?",
-    "What are some effective strategies for time management?",
-    "Where can I find authentic Italian cuisine in London?",
-    "What are some tips for maintaining a healthy lifestyle?",
-    "Can you suggest three classic movies from the 20th century?",
-    "How can I develop good public speaking skills?",
-    "What are some unique cultural traditions in Japan?",
-    "Can you recommend three budget-friendly destinations for solo travelers?"
+        "What are some popular tourist attractions in New York City?",
+        # "How can I improve my English writing skills?",
+        # "Can you recommend three must-read books from the science fiction genre?",
+        # "What are some effective strategies for time management?",
+        # "Where can I find authentic Italian cuisine in London?",
+        # "What are some tips for maintaining a healthy lifestyle?",
+        # "Can you suggest three classic movies from the 20th century?",
+        # "How can I develop good public speaking skills?",
+        # "What are some unique cultural traditions in Japan?",
+        # "Can you recommend three budget-friendly destinations for solo travelers?"
     ]
 
     prompts = en_prompts + de_prompts
 
-    candidate_premature_layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,29,30]
+    candidate_premature_layers = list(
+        range(model.config.num_hidden_layers + 1)
+    )  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,29,30]
 
     candidate_langs = ["en", "de"]
 
-    candidate_layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,29,30]
+    candidate_layers = list(
+        range(model.config.num_hidden_layers + 1)
+    )  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,29,30]
 
     lst_lang_distribution = []
     for prompt in tqdm(prompts):
