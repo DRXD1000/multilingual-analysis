26cd7a92c46af7ba9f2238d883174b2e90ef950f
Matthias Uhlig <matthias.uhlig@aol.de>
2025-01-01 20:05:45 +0100

    File Naming

--- src/multilingual_analysis/layers/test_layer.py
+++ src/multilingual_analysis/layers/test_layer.py
@@ -141,7 +141,7 @@ def plot_lang_distribution(lang_distribution, candidate_langs, candidate_layers,
     plt.title("Layerwise Language Distribution")
     plt.xlabel("Layer")
     plt.ylabel("Language")
-    plt.savefig(image_dir / "lang_distribution.png")
+    plt.savefig(image_dir / model_name.replace("/","-") +"_lang_distribution.png")
     if show_plot:
         plt.show()
 
@@ -149,31 +149,31 @@ def plot_lang_distribution(lang_distribution, candidate_langs, candidate_layers,
 def main(argv) -> None:
     de_prompts = [
         "Frage: Was sind die besten deutschen Filme? Antwort: ",
-        # "Frage: Was sind die besten deutschen Bücher? Antwort: ",
-        # "Frage: Wie kann ich meine Deutschkenntnisse verbessern? Antwort: ",
-        # "Kann mir jemand ein gutes Restaurant in München empfehlen?",
-        # "Was sind die besten Sehenswürdigkeiten in Berlin?",
-        # "Wie kann ich meine Deutschkenntnisse verbessern?",
-        # "Was sind die besten Tipps für einen guten Schlaf?",
-        # "Wo kann ich in Hamburg gut shoppen?",
-        # "Wie kann ich meine Zeit besser nutzen?",
-        # "Was sind die besten deutschen Serien?",
-        # "Was sind die besten deutschen Lieder?"
-        # "Kannst du drei günstige Reiseziele in Österreich für Einzelreisende empfehlen?",
-        # "Was sind effektive Strategien zur Stressbewältigung?"
+        "Frage: Was sind die besten deutschen Bücher? Antwort: ",
+        "Frage: Wie kann ich meine Deutschkenntnisse verbessern? Antwort: ",
+        "Kann mir jemand ein gutes Restaurant in München empfehlen?",
+        "Was sind die besten Sehenswürdigkeiten in Berlin?",
+        "Wie kann ich meine Deutschkenntnisse verbessern?",
+        "Was sind die besten Tipps für einen guten Schlaf?",
+        "Wo kann ich in Hamburg gut shoppen?",
+        "Wie kann ich meine Zeit besser nutzen?",
+        "Was sind die besten deutschen Serien?",
+        "Was sind die besten deutschen Lieder?"
+        "Kannst du drei günstige Reiseziele in Österreich für Einzelreisende empfehlen?",
+        "Was sind effektive Strategien zur Stressbewältigung?"
     ]
 
     en_prompts = [
         "What are some popular tourist attractions in New York City?",
-        # "How can I improve my English writing skills?",
-        # "Can you recommend three must-read books from the science fiction genre?",
-        # "What are some effective strategies for time management?",
-        # "Where can I find authentic Italian cuisine in London?",
-        # "What are some tips for maintaining a healthy lifestyle?",
-        # "Can you suggest three classic movies from the 20th century?",
-        # "How can I develop good public speaking skills?",
-        # "What are some unique cultural traditions in Japan?",
-        # "Can you recommend three budget-friendly destinations for solo travelers?"
+        "How can I improve my English writing skills?",
+        "Can you recommend three must-read books from the science fiction genre?",
+        "What are some effective strategies for time management?",
+        "Where can I find authentic Italian cuisine in London?",
+        "What are some tips for maintaining a healthy lifestyle?",
+        "Can you suggest three classic movies from the 20th century?",
+        "How can I develop good public speaking skills?",
+        "What are some unique cultural traditions in Japan?",
+        "Can you recommend three budget-friendly destinations for solo travelers?"
     ]
 
     prompts = en_prompts + de_prompts
